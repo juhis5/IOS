dffa2612f4b303445bc74f34fa581e08d9c33011
developer <developer@kc408mac08.tamk.ad>
2016-10-05 11:14:50 +0300

    Started weather app

---
+++ excercises/weather/weather/ForecastViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ForecastViewController.swift
+//  weather
+//
+//  Created by developer on 05/10/16.
+//  Copyright © 2016 Juho Lahtinen. All rights reserved.
+//
+
+import UIKit
+
+class ForecastViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
---
+++ excercises/weather/weather/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by developer on 05/10/16.
+//  Copyright © 2016 Juho Lahtinen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var weatherImg: UIImageView!
+    @IBOutlet weak var tempLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
